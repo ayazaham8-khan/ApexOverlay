ec5ee986d69d25b70e8f8468f0f3d3778ea13767
ayazaham8-khan <ayazaham8@gmail.com>
2026-08-15 14:53:04 +0500

    Create Color.kt

---
+++ app/src/main/java/com/novasphere/apexoverlay/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.novasphere.apexoverlay.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val ApexBackground = Color(0xFF0B0D12)
+val ApexSurface = Color(0xFF14171F)
+val ApexSurfaceElevated = Color(0xFF1B1F2A)
+val ApexAccent = Color(0xFF3DDCFF)
+val ApexAccentMuted = Color(0xFF2A8FA6)
+val ApexTextPrimary = Color(0xFFEDEFF4)
+val ApexTextSecondary = Color(0xFF8A90A0)
+val ApexBorder = Color(0xFF262B38)
