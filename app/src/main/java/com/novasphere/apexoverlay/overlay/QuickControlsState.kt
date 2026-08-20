7a7a55f35a2b6adebaaaabf81acd37ee8997ca3f
ayazaham8-khan <ayazaham8@gmail.com>
2026-08-20 11:44:00 +0500

    Update QuickControlsState.kt

--- app/src/main/java/com/novasphere/apexoverlay/overlay/QuickControlsState.kt
+++ app/src/main/java/com/novasphere/apexoverlay/overlay/QuickControlsState.kt
@@ -1,6 +1,8 @@
 package com.novasphere.apexoverlay.overlay
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
 /**
  * Tracks whether the in-game Quick Controls panel is currently open.
